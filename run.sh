821387e699772cecf1c9c8e31388671f8b13d699
Mike Seddon <seddonm1@gmail.com>
2020-04-09 19:01:40 +1000

    update version

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker run \
 --volume $(pwd)/examples:/home/jovyan/examples:Z \
 --env "ETL_CONF_ENV=production" \
 -p 4040:4040 \
-triplai/arc:arc_2.9.0_spark_2.4.5_scala_2.12_hadoop_2.9.2_1.0.0 \
+triplai/arc:arc_2.9.0_spark_2.4.5_scala_2.12_hadoop_2.9.2_1.1.0 \
 bin/spark-submit \
 --master local[*] \
 --driver-memory 4g \

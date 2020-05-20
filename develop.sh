0f9bf1f66b6f5ea142740e22c07653e11c46721e
Mike Seddon <seddonm1@gmail.com>
2020-05-21 07:53:15 +1000

    bump versions and fix memory allocation for jupyter

--- develop.sh
+++ develop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-ARC_JUPYTER_VERSION=2.3.3
+ARC_JUPYTER_VERSION=2.4.0
 HADOOP_VERSION=2.9.2
 IMAGE_VERSION=1.0.0
-JAVA_OPTS=-Xmx4g
+JAVA_OPTS=-Xmx4G
 cat <<"EOF"
 
-    ___                     __                  __           
+    ___                     __                  __
    /   |  __________       / /_  ______  __  __/ /____  _____
   / /| | / ___/ ___/  __  / / / / / __ \/ / / / __/ _ \/ ___/
- / ___ |/ /  / /__   / /_/ / /_/ / /_/ / /_/ / /_/  __/ /    
-/_/  |_/_/   \___/   \____/\__,_/ .___/\__, /\__/\___/_/     
-                               /_/    /____/                 
+ / ___ |/ /  / /__   / /_/ / /_/ / /_/ / /_/ / /_/  __/ /
+/_/  |_/_/   \___/   \____/\__,_/ .___/\__, /\__/\___/_/
+                               /_/    /____/
 EOF
 cat <<EOF
 version: ${ARC_JUPYTER_VERSION}
@@ -22,9 +22,9 @@ docker run \
 --name arc-jupyter \
 --rm \
 --volume $(pwd)/examples:/home/jovyan/examples:Z \
---env ${JAVA_OPTS} \
+--env "JAVA_OPTS=${JAVA_OPTS}" \
 --env "conf_spark_sql_extensions=io.delta.sql.DeltaSparkSessionExtension" \
---entrypoint='' \
+--entrypoint="" \
 --publish 4040:4040 \
 --publish 8888:8888 \
 triplai/arc-jupyter:arc-jupyter_${ARC_JUPYTER_VERSION}_scala_2.12_hadoop_${HADOOP_VERSION}_${IMAGE_VERSION} \

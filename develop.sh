fcbd223273365d25c6858aeef456c71b7597140d
Mike Seddon <seddonm1@gmail.com>
2020-10-09 09:32:54 +1100

    update run file

--- develop.sh
+++ develop.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
-JAVA_OPTS=-Xmx4G
-cat <<"EOF"
-
-    ___                     __                  __
-   /   |  __________       / /_  ______  __  __/ /____  _____
-  / /| | / ___/ ___/  __  / / / / / __ \/ / / / __/ _ \/ ___/
- / ___ |/ /  / /__   / /_/ / /_/ / /_/ / /_/ / /_/  __/ /
-/_/  |_/_/   \___/   \____/\__,_/ .___/\__, /\__/\___/_/
-                               /_/    /____/
-
-EOF
-
 docker run \
+--interactive \
+--tty \
 --name arc-jupyter \
 --rm \
 --volume $(pwd)/examples:/home/jovyan/examples:Z \
---env "JAVA_OPTS=${JAVA_OPTS}" \
---entrypoint="" \
+--env "JAVA_OPTS=-Xmx4G" \
 --publish 4040:4040 \
---publish 8888:8888 \
-triplai/arc-jupyter:latest \
-jupyter lab \
---ip=0.0.0.0 \
---no-browser \
---NotebookApp.password='' \
---NotebookApp.token=''
+--publish 8080:8080 \
+ghcr.io/tripl-ai/arc-jupyter:latest
--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ docker run \
 --rm \
 --volume $(pwd)/examples:/home/jovyan/examples:Z \
 --env "ETL_CONF_ENV=production" \
---entrypoint='' \
---publish 4041:4040 \
+--entrypoint="" \
+--publish 4040:4040 \
 triplai/arc:latest \
 bin/spark-submit \
 --master local[*] \
---driver-memory 4g \
+--driver-memory 4G \
 --driver-java-options "-XX:+UseG1GC" \
 --conf spark.authenticate.secret=$(openssl rand -hex 64) \
 --class ai.tripl.arc.ARC \
